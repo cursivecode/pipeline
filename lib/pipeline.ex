3543d687deb6b96a50b3eb9c22687aaff6ef4e57
cursivecode <cursivecode@gmail.com>
2014-12-05 23:52:37 -0500

    create function to setup pipeline for phoenix

--- lib/pipeline.ex
+++ lib/pipeline.ex
@@ -1,2 +1,27 @@
-defmodule Pipeline do
+defmodule Pipeline.CLI do
+	def main(project_root) do
+		if !File.dir? "#{project_root}/priv/assets" do
+			File.mkdir! "#{project_root}/priv/assets"
+			File.mkdir!"#{project_root}/priv/assets/coffeescripts"
+			File.mkdir! "#{project_root}/priv/assets/javascripts"
+			File.mkdir! "#{project_root}/priv/assets/stylesheets"
+			File.cp! "templates/stylesheets/phoenix.scss", "#{project_root}/priv/assets/stylesheets/phoenix.scss"
+			File.cp! "templates/javascripts/main.js", "#{project_root}/priv/assets/javascripts/main.js"
+			File.cp! "templates/coffeescripts/main.coffee", "#{project_root}/priv/assets/coffeescripts/main.coffee"
+		end
+		File.cp! "templates/gulp/gulpfile.js", "#{project_root}/gulpfile.js"
+		File.cp! "templates/node/package.json", "#{project_root}/package.json"
+		File.cp! "templates/layout/application.html.eex", "#{project_root}/web/templates/layout/application.html.eex"
+		File.cd! project_root
+		System.cmd "npm", ~w(install --save-dev gulp 
+												gulp-uglify 
+			                                    gulp-sass 
+			                                    gulp-plumber 
+			                                    gulp-livereload 
+			                                    gulp-shell
+			                                    gulp-concat 
+			                                    gulp-sourcemaps 
+			                                    gulp-coffee)
+	end
+	
 end
